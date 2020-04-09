472e23d4c10b3cbd0ec7655231e74bd5c46cecc1
Dr. Hans-Thomas Nürnberg <nuernberg@me.com>
2020-04-09 19:12:34 +0200

    Initial Commit

---
+++ DriftingWheels/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DriftingWheels
+//
+//  Created by Dr. Nürnberg on 09.04.20.
+//  Copyright © 2020 G2323. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
